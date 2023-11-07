3899dad811372dba48939948f74a53619f912822
Vinay Kakade <vinaykakade@gmail.com>
2023-11-07 14:27:40 +0530

    Remove unused install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# List and install whl files in gen_rust_lib
-search_dir=fennel/test_lib/gen_rust_lib
-for entry in "$search_dir"/*
-do
-  echo "Installing $entry"
-  pip3 install $entry --force-reinstall
-done
