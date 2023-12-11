d3ee0da11ec89e5d3e5bd2b75ffe5598c4e8b5f8
Zaki Siddiqui <zaki@fennel.ai>
2023-12-11 14:29:32 -0800

    regenerated protos

--- fennel/gen/connector_pb2.pyi
+++ fennel/gen/connector_pb2.pyi
@@ -12,16 +12,13 @@ database
 5. This whole module is called connector.
 """
 import builtins
-import collections.abc
 import google.protobuf.descriptor
 import google.protobuf.duration_pb2
-import google.protobuf.internal.containers
 import google.protobuf.internal.enum_type_wrapper
 import google.protobuf.message
 import google.protobuf.timestamp_pb2
 import google.protobuf.wrappers_pb2
 import kinesis_pb2
-import schema_pb2
 import schema_registry_pb2
 import sys
 import typing
@@ -645,27 +642,6 @@ class KinesisStream(google.protobuf.message.Message):
 global___KinesisStream = KinesisStream
 
 @typing_extensions.final
-class PreProcValue(google.protobuf.message.Message):
-    DESCRIPTOR: google.protobuf.descriptor.Descriptor
-
-    REF_FIELD_NUMBER: builtins.int
-    VALUE_FIELD_NUMBER: builtins.int
-    ref: builtins.str
-    @property
-    def value(self) -> schema_pb2.Value: ...
-    def __init__(
-        self,
-        *,
-        ref: builtins.str = ...,
-        value: schema_pb2.Value | None = ...,
-    ) -> None: ...
-    def HasField(self, field_name: typing_extensions.Literal["ref", b"ref", "value", b"value", "variant", b"variant"]) -> builtins.bool: ...
-    def ClearField(self, field_name: typing_extensions.Literal["ref", b"ref", "value", b"value", "variant", b"variant"]) -> None: ...
-    def WhichOneof(self, oneof_group: typing_extensions.Literal["variant", b"variant"]) -> typing_extensions.Literal["ref", "value"] | None: ...
-
-global___PreProcValue = PreProcValue
-
-@typing_extensions.final
 class Source(google.protobuf.message.Message):
     """-----------------------------------------
     Finally, all the sources and sinks
@@ -673,24 +649,6 @@ class Source(google.protobuf.message.Message):
     """
 
     DESCRIPTOR: google.protobuf.descriptor.Descriptor
-
-    @typing_extensions.final
-    class PreProcEntry(google.protobuf.message.Message):
-        DESCRIPTOR: google.protobuf.descriptor.Descriptor
-
-        KEY_FIELD_NUMBER: builtins.int
-        VALUE_FIELD_NUMBER: builtins.int
-        key: builtins.str
-        @property
-        def value(self) -> global___PreProcValue: ...
-        def __init__(
-            self,
-            *,
-            key: builtins.str = ...,
-            value: global___PreProcValue | None = ...,
-        ) -> None: ...
-        def HasField(self, field_name: typing_extensions.Literal["value", b"value"]) -> builtins.bool: ...
-        def ClearField(self, field_name: typing_extensions.Literal["key", b"key", "value", b"value"]) -> None: ...
 
     TABLE_FIELD_NUMBER: builtins.int
     DATASET_FIELD_NUMBER: builtins.int
@@ -700,7 +658,6 @@ class Source(google.protobuf.message.Message):
     TIMESTAMP_FIELD_FIELD_NUMBER: builtins.int
     CDC_FIELD_NUMBER: builtins.int
     STARTING_FROM_FIELD_NUMBER: builtins.int
-    PRE_PROC_FIELD_NUMBER: builtins.int
     @property
     def table(self) -> global___ExtTable: ...
     dataset: builtins.str
@@ -713,8 +670,6 @@ class Source(google.protobuf.message.Message):
     cdc: global___CDCStrategy.ValueType
     @property
     def starting_from(self) -> google.protobuf.timestamp_pb2.Timestamp: ...
-    @property
-    def pre_proc(self) -> google.protobuf.internal.containers.MessageMap[builtins.str, global___PreProcValue]: ...
     def __init__(
         self,
         *,
@@ -726,10 +681,9 @@ class Source(google.protobuf.message.Message):
         timestamp_field: builtins.str = ...,
         cdc: global___CDCStrategy.ValueType = ...,
         starting_from: google.protobuf.timestamp_pb2.Timestamp | None = ...,
-        pre_proc: collections.abc.Mapping[builtins.str, global___PreProcValue] | None = ...,
     ) -> None: ...
     def HasField(self, field_name: typing_extensions.Literal["_cursor", b"_cursor", "cursor", b"cursor", "every", b"every", "lateness", b"lateness", "starting_from", b"starting_from", "table", b"table"]) -> builtins.bool: ...
-    def ClearField(self, field_name: typing_extensions.Literal["_cursor", b"_cursor", "cdc", b"cdc", "cursor", b"cursor", "dataset", b"dataset", "every", b"every", "lateness", b"lateness", "pre_proc", b"pre_proc", "starting_from", b"starting_from", "table", b"table", "timestamp_field", b"timestamp_field"]) -> None: ...
+    def ClearField(self, field_name: typing_extensions.Literal["_cursor", b"_cursor", "cdc", b"cdc", "cursor", b"cursor", "dataset", b"dataset", "every", b"every", "lateness", b"lateness", "starting_from", b"starting_from", "table", b"table", "timestamp_field", b"timestamp_field"]) -> None: ...
     def WhichOneof(self, oneof_group: typing_extensions.Literal["_cursor", b"_cursor"]) -> typing_extensions.Literal["cursor"] | None: ...
 
 global___Source = Source
--- fennel/gen/format_pb2.py
+++ fennel/gen/format_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0c\x66ormat.proto\x12\x13\x66\x65nnel.proto.format\"\xb4\x03\n\nFileFormat\x12:\n\x07parquet\x18\x01 \x01(\x0b\x32\'.fennel.proto.format.FileFormat.ParquetH\x00\x12\x34\n\x04json\x18\x02 \x01(\x0b\x32$.fennel.proto.format.FileFormat.JsonH\x00\x12\x34\n\x04\x61vro\x18\x03 \x01(\x0b\x32$.fennel.proto.format.FileFormat.AvroH\x00\x12\x34\n\x04hudi\x18\x04 \x01(\x0b\x32$.fennel.proto.format.FileFormat.HudiH\x00\x12\x41\n\x0b\x64\x65lta_table\x18\x05 \x01(\x0b\x32*.fennel.proto.format.FileFormat.DeltaTableH\x00\x12\x32\n\x03\x63sv\x18\x06 \x01(\x0b\x32#.fennel.proto.format.FileFormat.CSVH\x00\x1a\t\n\x07Parquet\x1a\x06\n\x04Json\x1a\x06\n\x04\x41vro\x1a\x06\n\x04Hudi\x1a\x0c\n\nDeltaTable\x1a\x18\n\x03\x43SV\x12\x11\n\tdelimiter\x18\x01 \x01(\x0c\x42\x06\n\x04kindb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0c\x66ormat.proto\x12\x13\x66\x65nnel.proto.format\"\xf8\x03\n\nFileFormat\x12:\n\x07parquet\x18\x01 \x01(\x0b\x32\'.fennel.proto.format.FileFormat.ParquetH\x00\x12\x34\n\x04json\x18\x02 \x01(\x0b\x32$.fennel.proto.format.FileFormat.JsonH\x00\x12\x34\n\x04\x61vro\x18\x03 \x01(\x0b\x32$.fennel.proto.format.FileFormat.AvroH\x00\x12\x34\n\x04hudi\x18\x04 \x01(\x0b\x32$.fennel.proto.format.FileFormat.HudiH\x00\x12\x41\n\x0b\x64\x65lta_table\x18\x05 \x01(\x0b\x32*.fennel.proto.format.FileFormat.DeltaTableH\x00\x12\x32\n\x03\x63sv\x18\x06 \x01(\x0b\x32#.fennel.proto.format.FileFormat.CSVH\x00\x12\x38\n\x06pandas\x18\x07 \x01(\x0b\x32&.fennel.proto.format.FileFormat.PandasH\x00\x1a\t\n\x07Parquet\x1a\x06\n\x04Json\x1a\x06\n\x04\x41vro\x1a\x06\n\x04Hudi\x1a\x0c\n\nDeltaTable\x1a\x18\n\x03\x43SV\x12\x11\n\tdelimiter\x18\x01 \x01(\x0c\x1a\x08\n\x06PandasB\x06\n\x04kindb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -21,17 +21,19 @@ _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'format_pb2', _globals)
 if _descriptor._USE_C_DESCRIPTORS == False:
   DESCRIPTOR._options = None
   _globals['_FILEFORMAT']._serialized_start=38
-  _globals['_FILEFORMAT']._serialized_end=474
-  _globals['_FILEFORMAT_PARQUET']._serialized_start=393
-  _globals['_FILEFORMAT_PARQUET']._serialized_end=402
-  _globals['_FILEFORMAT_JSON']._serialized_start=404
-  _globals['_FILEFORMAT_JSON']._serialized_end=410
-  _globals['_FILEFORMAT_AVRO']._serialized_start=412
-  _globals['_FILEFORMAT_AVRO']._serialized_end=418
-  _globals['_FILEFORMAT_HUDI']._serialized_start=420
-  _globals['_FILEFORMAT_HUDI']._serialized_end=426
-  _globals['_FILEFORMAT_DELTATABLE']._serialized_start=428
-  _globals['_FILEFORMAT_DELTATABLE']._serialized_end=440
-  _globals['_FILEFORMAT_CSV']._serialized_start=442
-  _globals['_FILEFORMAT_CSV']._serialized_end=466
+  _globals['_FILEFORMAT']._serialized_end=542
+  _globals['_FILEFORMAT_PARQUET']._serialized_start=451
+  _globals['_FILEFORMAT_PARQUET']._serialized_end=460
+  _globals['_FILEFORMAT_JSON']._serialized_start=462
+  _globals['_FILEFORMAT_JSON']._serialized_end=468
+  _globals['_FILEFORMAT_AVRO']._serialized_start=470
+  _globals['_FILEFORMAT_AVRO']._serialized_end=476
+  _globals['_FILEFORMAT_HUDI']._serialized_start=478
+  _globals['_FILEFORMAT_HUDI']._serialized_end=484
+  _globals['_FILEFORMAT_DELTATABLE']._serialized_start=486
+  _globals['_FILEFORMAT_DELTATABLE']._serialized_end=498
+  _globals['_FILEFORMAT_CSV']._serialized_start=500
+  _globals['_FILEFORMAT_CSV']._serialized_end=524
+  _globals['_FILEFORMAT_PANDAS']._serialized_start=526
+  _globals['_FILEFORMAT_PANDAS']._serialized_end=534
 # @@protoc_insertion_point(module_scope)
