e921b79a994a6832e348f4e98f2b284fe4257aaf
Rahul Khanna <r.khanna237@gmail.com>
2025-02-03 19:58:38 +0530

    Add documentation for xray

---
+++ docs/examples/api-reference/client/xray.py
@@ -0,0 +1,12 @@
+from fennel.testing import mock
+
+@mock
+def test_basic(client):
+    def xray_client():
+        # docsnip xray
+        client.xray(
+            datasets=["Dataset1", "Dataset2"],
+            featuresets=[],
+            properties=["backlog", "sink_destination", "name"]
+        )  # docsnip-highlight
+        # /docsnip
